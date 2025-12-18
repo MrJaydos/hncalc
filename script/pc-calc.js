b518c04bfa23a86bdd8c606bffb53ae7d817ef46
Jayden Dickinson <jaydend93@gmail.com>
2025-12-18 15:05:48 +1100

    Adding calculations for PC

--- script/pc-calc.js
+++ script/pc-calc.js
@@ -17,3 +17,27 @@ function goToResults() {
   //Redirect to results page.
   window.location.href = "pc-calc-results.html";
 }
+
+function calculateCare(rrp, discount, category) {
+  rrp = Number(rrp);
+  discount = Number(discount || 0);
+    // Add extra categories by duplicating the below if statement along with the nested if statement.
+  let care;
+  if (category === "computer") {
+    if (rrp <= 299) care = { y1: 49, y2: 69, y3: 89 };
+    else if (rrp <= 499) care = { y1: 69, y2: 119, y3: 169 };
+    else care = { y1: "TBD", y2: "TBD", y3: "TBD" };
+  } else if (category === "camera") {
+    if (rrp <= 299) care = { y1: 49, y2: 69, y3: 89 };
+    else if (rrp <= 499) care = { y1: 69, y2: 119, y3: 169 };
+    else care = { y1: "TBD", y2: "TBD", y3: "TBD" };
+  } else care = { y1: "TBD", y2: "TBD", y3: "TBD" };
+
+  // Estimated cost (internal only)
+  const costPercent = { y1: 0.40, y2: 0.50, y3: 0.55 };
+  const estimatedCost = {
+    y1: (typeof care.y1 === 'number') ? care.y1 * costPercent.y1 : null,
+    y2: (typeof care.y2 === 'number') ? care.y2 * costPercent.y2 : null,
+    y3: (typeof care.y3 === 'number') ? care.y3 * costPercent.y3 : null
+  };
+}
