3b65d96dbe337e0419e2bedf15dd7a150e8fbae5
Jayden Dickinson <jaydend93@gmail.com>
2025-12-18 14:46:34 +1100

    Formating issues

--- script/pc-calc.js
+++ script/pc-calc.js
@@ -1,19 +1,19 @@
 function goToResults() {
-    //Assign consts based on inputs on the page
-    const rrp = document.getElementById('rrp').value;
-    const discount = document.getElementById('discount').value || 0;
-    const category = document.getElementById('category').value;
+  //Assign consts based on inputs on the page
+  const rrp = document.getElementById("rrp").value;
+  const discount = document.getElementById("discount").value || 0;
+  const category = document.getElementById("category").value;
 
-    if (!rrp) {
-        alert('Please enter the product RRP.');
-        return;
-    }
+  if (!rrp) {
+    alert("Please enter the product RRP.");
+    return;
+  }
 
-    //Save Inputs to local storage
-    localStorage.setItem('rrp', rrp);
-    localStorage.setItem('discount', discount)
-    localStorage.setItem('category', category)
+  //Save Inputs to local storage
+  localStorage.setItem("rrp", rrp);
+  localStorage.setItem("discount", discount);
+  localStorage.setItem("category", category);
 
-    //Redirect to results page.
-    window.location.href = 'pc-calc-results.html'
+  //Redirect to results page.
+  window.location.href = "pc-calc-results.html";
 }
